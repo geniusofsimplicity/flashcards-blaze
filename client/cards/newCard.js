55d5169b3ffd2ecce84f5d116a80533fbe757444
Pasha <genius.of.simplicity@gmail.com>
2016-12-11 11:18:16 +0100

    make cardsByCategory work

--- client/cards/newCard.js
+++ client/cards/newCard.js
@@ -4,9 +4,9 @@ Meteor.subscribe("categories");
 Template.NewCard.events({
 	'submit #new-card-form': function(e) {
 		e.preventDefault();		
-		frontSide = e.target.frontSide.value;
-		backSide = e.target.backSide.value;
-		categoryName = e.target.categoryName.value;
+		let frontSide = e.target.frontSide.value;
+		let backSide = e.target.backSide.value;
+		let categoryName = e.target.categoryName.value;
 		Meteor.call('addCategory', categoryName);
 		let categoryId = Categories.findOne({ name: categoryName })._id;
 		let newCard = { frontSide, backSide , categories: [{ categoryId: categoryId }] };		
