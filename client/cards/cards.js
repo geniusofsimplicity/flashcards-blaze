df99d255b8a722642dd91ab0667c3a1f6a1ab6af
Pasha <genius.of.simplicity@gmail.com>
2016-12-14 11:46:12 +0100

    let limit recently created cards

--- client/cards/cards.js
+++ client/cards/cards.js
@@ -1,18 +1,30 @@
 Template.CardsAll.onCreated(function() {
 	Session.set('editCardAllow', true);
+	let amount = parseInt(Session.get('recentlyCreatedAmount'));
+	if(amount == 0) {
+		Session.set('recentlyCreatedAmount', 5);
+	}
+	
 });
 
 Meteor.subscribe('cards');
 
 Template.CardsAll.helpers({
 	cards: () => {
-		return Cards.find({});
+		let amount = parseInt(Session.get('recentlyCreatedAmount'));
+		return Cards.find({}, { limit: amount , sort: { 'createdAt': -1 } });
+	},
+	recentlyCreatedAmount: () => {
+		return parseInt(Session.get('recentlyCreatedAmount'));
 	}
 });
 
 Template.CardsAll.events({
 	'click .new-card': () => {
 		Session.set('newCard', true);
-		// Session.set('editCardAllow', true);
+	},
+	'change input#recently-created-amount': (e) => {
+		let amount = e.target.value;
+		Session.set('recentlyCreatedAmount', amount);
 	}
 });
